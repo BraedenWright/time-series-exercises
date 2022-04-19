093b348926c332bcfb621857e013ca4b539f5e71
Braeden Wright <braeden.wright21@gmail.com>
2022-04-19 11:41:33 -0500

    main

--- prepare.py
+++ prepare.py
@@ -34,3 +34,39 @@ def prep_opsd_data(df):
     df = df.fillna(0)
     df['wind_solar'] = df.wind + df.solar
     return df
+
+
+
+# provided by Codeup
+
+def evaluate(target_var):
+    '''
+    This function will take the actual values of the target_var from validate, 
+    and the predicted values stored in yhat_df, 
+    and compute the rmse, rounding to 0 decimal places. 
+    it will return the rmse. 
+    '''
+    rmse = round(sqrt(mean_squared_error(validate[target_var], yhat_df[target_var])), 0)
+    return rmse
+
+
+
+
+# provided by Codeup
+
+
+def plot_and_eval(target_var):
+    '''
+    This function takes in the target var name (string), and returns a plot
+    of the values of train for that variable, validate, and the predicted values from yhat_df. 
+    it will als lable the rmse. 
+    '''
+    plt.figure(figsize = (12,4))
+    plt.plot(train[target_var], label='Train', linewidth=1)
+    plt.plot(validate[target_var], label='Validate', linewidth=1)
+    plt.plot(yhat_df[target_var])
+    plt.title(target_var)
+    rmse = evaluate(target_var)
+    print(target_var, '-- RMSE: {:.0f}'.format(rmse))
+    plt.show()
+
